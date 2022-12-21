14f9a9c013d472f15710831ef6922cb4a1589ac1
Rayncor <sowa332@gmail.com>
2022-12-21 12:40:48 -0800

    trying to figure out console log miscalc in reduce method

--- reduce.js
+++ reduce.js
@@ -16,3 +16,13 @@ console.log(sumOfNum);
 
 //the same thing can be done with reduce- let me figure out how. 
 
+const newNumArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+//while you don't actually HAVE to use an initial value, the intial value 0 is good for numbers because if you don't the first item in your array will be used, which can cause issues.
+
+const newSumOfNum = newNumArr.reduce((acc, num) => num < 8 ? acc : acc += num, 0);
+
+//i am not sure why this is logging 27 instead of 28. it has something to do with the initial value being set. 
+
+console.log(newSumOfNum);
